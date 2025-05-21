d5c344cd59abf8a3d1de1144c4ff51a2ee3377a5
Dahlia <embracedahlia@gmail.com>
2025-05-21 00:19:43 -0500

    feat: enable cors

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,19 @@
 import express, { Request, Response, NextFunction } from 'express';
 import productRouter from './routes/productRoute';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 app.set("port", process.env.PORT || 3003);
 const port = app.get("port");
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  methods: "GET, POST, PUT, DELETE",
+  allowedHeaders: "Content-Type, Authorization"
+}));
 
 app.use(express.json());
 
